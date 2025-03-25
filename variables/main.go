57239bcb1bfbb8b4d6594197adb33311b4af4220
Karan <mrkaran2k5@gmail.com>
2025-03-25 22:09:06 +0530

    Variables in Go

---
+++ variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Karan Aggarwal"
+	// var name = "Karan Aggarwal"
+
+	// Type inference
+	name := "Karan Aggarwal"
+
+	var fullname string
+
+	fullname = "Karan Aggarwal"
+
+	var cgpa float32 = 10.0
+	// var cgpa = 10.0
+	// cgpa := 10.0
+
+	fmt.Println(name, "", fullname, "has CGPA", cgpa)
+}
